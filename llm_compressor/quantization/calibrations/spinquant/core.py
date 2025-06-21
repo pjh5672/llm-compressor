fix: Activate weight quantizer in forward with Rotation matrix

llm_compressor/quantization/calibrations/spinquant/core.py
@@ -31,6 +31,7 @@ from quantization.calibrations.spinquant.rotation_utils import (
 from quantization.calibrations.spinquant.fuse_norm_utils import fuse_layer_norms  # noqa: E402
 from quantization.calibrations.spinquant.optimizer import SGDG  # noqa: E402
 from utils.dataset import CustomJsonDataset  # noqa: E402
+from utils.torch_utils import cleanup_memory  # noqa: E402
 
 
 class RotateModule(nn.Module):
@@ -64,21 +65,21 @@ def spinquant(
         quant_config = kwargs.get("quant_config")
 
         if isinstance(model, LlamaForCausalLM):
-            model = SpinLlamaForCausalLM.from_pretrained(
+            spin_model = SpinLlamaForCausalLM.from_pretrained(
                 model_path,
                 attn_implementation="eager",
                 torch_dtype=torch.float32,
                 device_map="auto",
             )
         elif isinstance(model, Qwen2ForCausalLM):
-            model = SpinQwen2ForCausalLM.from_pretrained(
+            spin_model = SpinQwen2ForCausalLM.from_pretrained(
                 model_path,
                 attn_implementation="eager",
                 torch_dtype=torch.float32,
                 device_map="auto",
             )
         elif isinstance(model, Qwen3ForCausalLM):
-            model = SpinQwen3ForCausalLM.from_pretrained(
+            spin_model = SpinQwen3ForCausalLM.from_pretrained(
                 model_path,
                 attn_implementation="eager",
                 torch_dtype=torch.float32,
@@ -87,27 +88,33 @@ def spinquant(
         else:
             raise RuntimeError(f"Not support model yet, got {model_path}")
 
-        model._prepare_model(quant_config=quant_config)
+        spin_model._prepare_model(quant_config=quant_config, mse=mse)
 
-        process_word_embeddings = model.config.tie_word_embeddings
+        process_word_embeddings = spin_model.config.tie_word_embeddings
         if process_word_embeddings:
-            model.config.tie_word_embeddings = False
-            model.lm_head.weight.data = model.model.embed_tokens.weight.data.clone()
+            spin_model.config.tie_word_embeddings = False
+            spin_model.lm_head.weight.data = (
+                spin_model.model.embed_tokens.weight.data.clone()
+            )
 
-        for param in model.parameters():
+        for param in spin_model.parameters():
             param.requires_grad = False
 
-        R1 = random_hadamard_matrix(model.config.hidden_size, model.device)
-        model.R1 = RotateModule(R1, model.device)
-        for i in range(model.config.num_hidden_layers):
-            head_dim = model.config.hidden_size // model.config.num_attention_heads
-            R2 = random_hadamard_matrix(head_dim, model.device)
-            model.model.layers[i].self_attn.R2 = RotateModule(R2, model.device)
+        R1 = random_hadamard_matrix(spin_model.config.hidden_size, spin_model.device)
+        spin_model.R1 = RotateModule(R1, spin_model.device)
+        for i in range(spin_model.config.num_hidden_layers):
+            head_dim = (
+                spin_model.config.hidden_size // spin_model.config.num_attention_heads
+            )
+            R2 = random_hadamard_matrix(head_dim, spin_model.device)
+            spin_model.model.layers[i].self_attn.R2 = RotateModule(
+                R2, spin_model.device
+            )
 
-        model.seqlen = seq_len
-        trainable_parameters = [model.R1.weight] + [
-            model.model.layers[i].self_attn.R2.weight
-            for i in range(model.config.num_hidden_layers)
+        spin_model.seqlen = seq_len
+        trainable_parameters = [spin_model.R1.weight] + [
+            spin_model.model.layers[i].self_attn.R2.weight
+            for i in range(spin_model.config.num_hidden_layers)
         ]
         tokenizer = AutoTokenizer.from_pretrained(
             pretrained_model_name_or_path=model_path,
@@ -119,7 +126,7 @@ def spinquant(
         )
         train_data = CustomJsonDataset(tokenizer=tokenizer, block_size=seq_len)
         training_args = TrainingArguments(
-            num_train_epochs=3,
+            num_train_epochs=0.2,
             per_device_train_batch_size=4,
             per_device_eval_batch_size=4,
         )
@@ -127,7 +134,7 @@ def spinquant(
             trainable_parameters, lr=training_args.learning_rate, stiefel=True
         )
         trainer = Trainer(
-            model=model,
+            model=spin_model,
             tokenizer=tokenizer,
             args=training_args,
             train_dataset=train_data,
@@ -143,6 +150,8 @@ def spinquant(
             if "R1.weight" in key or "self_attn.R2" in key
         }
         torch.save(R_dict, os.path.join(kwargs.get("save_path"), "R.bin"))
+        del spin_model
+        cleanup_memory(verbose=False)
 
     use_cache = model.config.use_cache
     model.config.use_cache = False

llm_compressor/quantization/calibrations/spinquant/modeling/llama.py
@@ -71,6 +71,7 @@ class SpinLlamaMLP(LlamaMLP):
         mlp: LlamaMLP,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             mlp.config,
@@ -84,6 +85,9 @@ class SpinLlamaMLP(LlamaMLP):
         self.down_proj = QLinear(
             linear=mlp.down_proj, quant_config=quant_config.linear, dtype=dtype
         )
+        self.gate_proj.weight_quantizer.mse = mse
+        self.up_proj.weight_quantizer.mse = mse
+        self.down_proj.weight_quantizer.mse = mse
 
     def forward(self, x, R1):
         down_proj = self.down_proj(
@@ -102,6 +106,7 @@ class SpinLlamaAttention(LlamaAttention):
         attention: LlamaAttention,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             attention.config,
@@ -121,6 +126,10 @@ class SpinLlamaAttention(LlamaAttention):
         self.o_proj = QLinear(
             linear=attention.o_proj, quant_config=quant_config.linear, dtype=dtype
         )
+        self.q_proj.weight_quantizer.mse = mse
+        self.k_proj.weight_quantizer.mse = mse
+        self.v_proj.weight_quantizer.mse = mse
+        self.o_proj.weight_quantizer.mse = mse
         self.R2 = None
 
     def forward(
@@ -188,13 +197,14 @@ class SpinLlamaDecoderLayer(LlamaDecoderLayer):
         decoder: LlamaDecoderLayer,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             config,
             layer_idx,
         )
-        self.self_attn = SpinLlamaAttention(decoder.self_attn, quant_config, dtype)
-        self.mlp = SpinLlamaMLP(decoder.mlp, quant_config, dtype)
+        self.self_attn = SpinLlamaAttention(decoder.self_attn, quant_config, dtype, mse)
+        self.mlp = SpinLlamaMLP(decoder.mlp, quant_config, dtype, mse)
         self.input_layernorm = decoder.input_layernorm
         self.post_attention_layernorm = decoder.post_attention_layernorm
 
@@ -252,6 +262,7 @@ class SpinLlamaModel(LlamaModel):
         model: LlamaModel,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(model.config)
 
@@ -259,7 +270,7 @@ class SpinLlamaModel(LlamaModel):
         self.layers = nn.ModuleList(
             [
                 SpinLlamaDecoderLayer(
-                    model.config, idx, self.layers[idx], quant_config, dtype
+                    model.config, idx, self.layers[idx], quant_config, dtype, mse
                 )
                 for idx in range(len(self.layers))
             ]
@@ -392,9 +403,12 @@ class SpinLlamaForCausalLM(LlamaForCausalLM):
     ):
         super().__init__(config)
 
-    def _prepare_model(self, quant_config):
-        self.model = SpinLlamaModel(self.model, quant_config, self.dtype)
+    def _prepare_model(self, quant_config, mse):
+        self.model = SpinLlamaModel(self.model, quant_config, self.dtype, mse)
         self.lm_head = QLinear(self.lm_head, quant_config.head, self.dtype)
+        self.lm_head.weight_quantizer.mse = mse
+        W = self.lm_head.weight.data
+        self.lm_head.weight.data = self.lm_head.weight_quantizer(W)
         self.R1 = None
 
     def forward(

llm_compressor/quantization/calibrations/spinquant/modeling/qwen2.py
@@ -71,6 +71,7 @@ class SpinQwen2MLP(Qwen2MLP):
         mlp: Qwen2MLP,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             mlp.config,
@@ -84,6 +85,9 @@ class SpinQwen2MLP(Qwen2MLP):
         self.down_proj = QLinear(
             linear=mlp.down_proj, quant_config=quant_config.linear, dtype=dtype
         )
+        self.gate_proj.weight_quantizer.mse = mse
+        self.up_proj.weight_quantizer.mse = mse
+        self.down_proj.weight_quantizer.mse = mse
 
     def forward(self, x, R1):
         down_proj = self.down_proj(
@@ -102,6 +106,7 @@ class SpinQwen2Attention(Qwen2Attention):
         attention: Qwen2Attention,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             attention.config,
@@ -121,6 +126,10 @@ class SpinQwen2Attention(Qwen2Attention):
         self.o_proj = QLinear(
             linear=attention.o_proj, quant_config=quant_config.linear, dtype=dtype
         )
+        self.q_proj.weight_quantizer.mse = mse
+        self.k_proj.weight_quantizer.mse = mse
+        self.v_proj.weight_quantizer.mse = mse
+        self.o_proj.weight_quantizer.mse = mse
         self.layer_types = [
             "sliding_attention"
             if attention.config.sliding_window is not None
@@ -201,13 +210,14 @@ class SpinQwen2DecoderLayer(Qwen2DecoderLayer):
         decoder: Qwen2DecoderLayer,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             config,
             layer_idx,
         )
-        self.self_attn = SpinQwen2Attention(decoder.self_attn, quant_config, dtype)
-        self.mlp = SpinQwen2MLP(decoder.mlp, quant_config, dtype)
+        self.self_attn = SpinQwen2Attention(decoder.self_attn, quant_config, dtype, mse)
+        self.mlp = SpinQwen2MLP(decoder.mlp, quant_config, dtype, mse)
         self.input_layernorm = decoder.input_layernorm
         self.post_attention_layernorm = decoder.post_attention_layernorm
 
@@ -265,6 +275,7 @@ class SpinQwen2Model(Qwen2Model):
         model: Qwen2Model,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(model.config)
 
@@ -272,7 +283,7 @@ class SpinQwen2Model(Qwen2Model):
         self.layers = nn.ModuleList(
             [
                 SpinQwen2DecoderLayer(
-                    model.config, idx, self.layers[idx], quant_config, dtype
+                    model.config, idx, self.layers[idx], quant_config, dtype, mse
                 )
                 for idx in range(len(self.layers))
             ]
@@ -405,9 +416,12 @@ class SpinQwen2ForCausalLM(Qwen2ForCausalLM):
     ):
         super().__init__(config)
 
-    def _prepare_model(self, quant_config):
-        self.model = SpinQwen2Model(self.model, quant_config, self.dtype)
+    def _prepare_model(self, quant_config, mse):
+        self.model = SpinQwen2Model(self.model, quant_config, self.dtype, mse)
         self.lm_head = QLinear(self.lm_head, quant_config.head, self.dtype)
+        self.lm_head.weight_quantizer.mse = mse
+        W = self.lm_head.weight.data
+        self.lm_head.weight.data = self.lm_head.weight_quantizer(W)
         self.R1 = None
 
     def forward(

llm_compressor/quantization/calibrations/spinquant/modeling/qwen3.py
@@ -71,6 +71,7 @@ class SpinQwen2MLP(Qwen3MLP):
         mlp: Qwen3MLP,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             mlp.config,
@@ -84,6 +85,9 @@ class SpinQwen2MLP(Qwen3MLP):
         self.down_proj = QLinear(
             linear=mlp.down_proj, quant_config=quant_config.linear, dtype=dtype
         )
+        self.gate_proj.weight_quantizer.mse = mse
+        self.up_proj.weight_quantizer.mse = mse
+        self.down_proj.weight_quantizer.mse = mse
 
     def forward(self, x, R1):
         down_proj = self.down_proj(
@@ -102,6 +106,7 @@ class SpinQwen3Attention(Qwen3Attention):
         attention: Qwen3Attention,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             attention.config,
@@ -121,6 +126,10 @@ class SpinQwen3Attention(Qwen3Attention):
         self.o_proj = QLinear(
             linear=attention.o_proj, quant_config=quant_config.linear, dtype=dtype
         )
+        self.q_proj.weight_quantizer.mse = mse
+        self.k_proj.weight_quantizer.mse = mse
+        self.v_proj.weight_quantizer.mse = mse
+        self.o_proj.weight_quantizer.mse = mse
         self.sliding_window = attention.sliding_window
         self.R2 = None
 
@@ -190,13 +199,14 @@ class SpinQwen3DecoderLayer(Qwen3DecoderLayer):
         decoder: Qwen3DecoderLayer,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(
             config,
             layer_idx,
         )
-        self.self_attn = SpinQwen3Attention(decoder.self_attn, quant_config, dtype)
-        self.mlp = SpinQwen2MLP(decoder.mlp, quant_config, dtype)
+        self.self_attn = SpinQwen3Attention(decoder.self_attn, quant_config, dtype, mse)
+        self.mlp = SpinQwen2MLP(decoder.mlp, quant_config, dtype, mse)
         self.input_layernorm = decoder.input_layernorm
         self.post_attention_layernorm = decoder.post_attention_layernorm
 
@@ -254,6 +264,7 @@ class SpinQwen3Model(Qwen3Model):
         model: Qwen3Model,
         quant_config,
         dtype,
+        mse=False,
     ):
         super().__init__(model.config)
 
@@ -261,7 +272,7 @@ class SpinQwen3Model(Qwen3Model):
         self.layers = nn.ModuleList(
             [
                 SpinQwen3DecoderLayer(
-                    model.config, idx, self.layers[idx], quant_config, dtype
+                    model.config, idx, self.layers[idx], quant_config, dtype, mse
                 )
                 for idx in range(len(self.layers))
             ]
@@ -394,9 +405,12 @@ class SpinQwen3ForCausalLM(Qwen3ForCausalLM):
     ):
         super().__init__(config)
 
-    def _prepare_model(self, quant_config):
-        self.model = SpinQwen3Model(self.model, quant_config, self.dtype)
+    def _prepare_model(self, quant_config, mse):
+        self.model = SpinQwen3Model(self.model, quant_config, self.dtype, mse)
         self.lm_head = QLinear(self.lm_head, quant_config.head, self.dtype)
+        self.lm_head.weight_quantizer.mse = mse
+        W = self.lm_head.weight.data
+        self.lm_head.weight.data = self.lm_head.weight_quantizer(W)
         self.R1 = None
 
     def forward(