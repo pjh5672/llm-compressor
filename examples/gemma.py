a4635ac325215a31fa0b30517461a3a6f142feca
Park Jiho <pjh5672.dev@gmail.com>
2025-07-15 10:08:47 +0900

    feat: Add example for Gemma

---
+++ examples/gemma.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+import torch
+from transformers import AutoTokenizer
+
+from llm_compressor.utils.args import build_parser
+from llm_compressor.utils.general import print_eval
+from llm_compressor.evaluation.eval import LMEvaluator
+from llm_compressor.models.gemma import CompressGemmaForCausalLM
+
+
+############### Init Arguments ###############
+ROOT = Path(__file__).resolve().parents[1]
+args, device = build_parser(ROOT)
+
+############### Model Definition ###############
+tokenizer = AutoTokenizer.from_pretrained(args.model)
+model = CompressGemmaForCausalLM.from_pretrained(
+    args.model,
+    attn_implementation="eager",
+    torch_dtype=torch.bfloat16,
+    device_map="cpu",
+)
+
+############### Model Pruning ###############
+model.prune(
+    tokenizer=tokenizer,
+    prune_method=args.prune_method,
+    prune_config=args.prune_config,
+    device=device,
+    prune=args.prune,
+)
+
+############### Model Profiling ###############
+if args.profile:
+    model.profile(
+        quant_config=args.quant_config,
+        device=device,
+        save_path=args.exp_dir,
+    )
+    args.qparser.disable_profile(args.quant_config)
+
+############### Model Quantization ###############
+quant_kwargs = {
+    "n_samples": args.calib_num,
+    "seq_len": args.seq_len,
+    "rotation_path": args.rotation_path,
+}
+model.quantize(
+    tokenizer=tokenizer,
+    quant_method=args.quant_method,
+    quant_config=args.quant_config,
+    device=device,
+    quantize=args.quantize,
+    **quant_kwargs,
+)
+
+############### Model Evaluation ###############
+evaluator = LMEvaluator(
+    model=model, 
+    device=device,
+    n_samples=128
+)
+eval_kwargs = {
+    "tokenizer_path": args.model,
+    "seq_len": args.seq_len,
+    "batch_size": args.batch_size,
+    "check_sparsity": args.prune,
+}
+results = evaluator.eval(tasks=args.tasks, **eval_kwargs)
+print_eval(results)
+
+############### Model Saving ###############
+if args.save_path is not None:
+    model.save_compressed(args.save_path)
